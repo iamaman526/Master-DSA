b0320ff5e2932d6745648643f56cc088747591fb
iamaman526 <iamaman526@gmail.com>
2023-03-11 21:54:01 +0530

    vectors

---
+++ Dsa-Upskill/Pairs&vectors/01.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdtr1c++.h>
+
+using namespace std;
+
+int main()
+{
+    // methods to intialize value
+    // 1
+    // pair<int, int> p = {1, 2};
+    // 2
+    //   pair<int,int>p = make_pair(1,2);
+    // 3
+    pair<int, int> p(1, 2);
+
+    cout << p.first << " " << p.second << endl;
+
+    pair<char , int>p2 = 
+}
