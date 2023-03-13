c9b99a49336980dc548df82abee95b2e6006d7cd
iamaman526 <iamaman526@gmail.com>
2023-03-13 11:11:45 +0530

    pairs-done

--- Dsa-Upskill/Pairs&vectors/01.cpp
+++ Dsa-Upskill/Pairs&vectors/01.cpp
@@ -4,15 +4,40 @@ using namespace std;
 
 int main()
 {
-    // methods to intialize value
+    // methods to intialize value with same data type
     // 1
     // pair<int, int> p = {1, 2};
     // 2
     //   pair<int,int>p = make_pair(1,2);
     // 3
-    pair<int, int> p(1, 2);
+    // pair<int, int> p(1, 2);
 
-    cout << p.first << " " << p.second << endl;
+    // with diff datatype
 
-    pair<char , int>p2 = 
+    // pair<char , int>p2 = {'a',3};
+    // cout<<p2.first<<" " << p2.second <<endl;
+
+    // pair< string , int>p3 = {"hello",5};
+    // cout<<p3.first<<" "<<p3.second<<endl;
+
+    // new copy of pair
+    // ampersand used for refrencing the pair
+
+    //  pair<int, int> &u = p;
+    //  u.first = 7;
+    //   cout << p.first << " " << p.second << endl;
+    //   cout << u.first << " " << u.second << endl;
+
+    // comparision
+    pair<int, int> p = {1, 2};
+    pair<int, int> u = {1, 2};
+
+    if (p < u)
+    {
+        cout << "yes" << endl;
+    }
+    if (p == u)
+    {
+        cout << "they are equal" << endl;
+    }
 }
